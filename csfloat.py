5f295ca02e2e668d571e52e747e9fed360b5e650
Tsk <dtyszkajr@gmail.com>
2025-09-27 21:10:34 -0300

    initial commit

---
+++ csfloat.py
@@ -0,0 +1,86 @@
+
+
+import os
+import csv
+import time
+import requests
+import datetime
+from dotenv import load_dotenv
+import tqdm
+
+load_dotenv()
+
+CSFLOAT_BASE_URL = "https://csfloat.com/api/v1"
+ORDERS_BOOK_FPATH = "data/orders_book.csv"
+HEADERS = {'Authorization': os.getenv("CSFLOAT_API_KEY")}
+
+
+def load_orders_book(fpath: str = ORDERS_BOOK_FPATH) -> list[dict]:
+    """Loads orders data into a list of dicts"""
+    data = []
+    with open(fpath, mode='r', newline='', encoding='utf-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
+        for row in csv_reader:
+            if row.get('paid'):
+                row['paid'] = float(row['paid'])
+            data.append(row)
+    return data
+
+
+def get_listings(skin_name: str, qty: int = 3) -> list[dict]:
+    """Obtain current selling listings for the item"""
+    payload = {'limit': qty, 'sort_by': 'lowest_price', 'type': 'buy_now', 'market_hash_name': skin_name}
+    req = requests.get(f'{CSFLOAT_BASE_URL}/listings', headers=HEADERS, params=payload)
+    req.raise_for_status()
+    return req.json()['data']
+
+
+def get_buy_orders(listing: dict, qty: int = 5) -> list[dict]:
+    """Obtains current buy orders for the listing"""
+    listing_id = listing.get('id')
+    payload = {'limit': qty}
+    req = requests.get(f'{CSFLOAT_BASE_URL}/listings/{listing_id}/buy-orders', headers=HEADERS, params=payload)
+    req.raise_for_status()
+    return req.json()
+
+
+def get_sales_graph(skin_name: str) -> list[dict]:
+    """Obtains historical data of avg/median sale price"""
+    req = requests.get(f'{CSFLOAT_BASE_URL}/history/{skin_name}/graph', headers=HEADERS)
+    req.raise_for_status()
+    return req.json()
+
+
+def gather_current_prices(orders_data: list[dict]) -> list[dict]:
+    """Collects current info of prices for each item from csfloat"""
+    sleep_time = 0.7
+    min_buy_orders_qty = 2
+    history_period = 3
+    current_prices_data = []
+    # obtaining list of unique names to avoid duplicated requests
+    unique_names = {i['name'] for i in orders_data}
+    # iterating over skins
+    for skin in tqdm.tqdm(unique_names, ncols=99):
+        listings = get_listings(skin)
+        lowest_sell_price = listings[0]['price'] / 100
+        estimated_price = listings[0]['reference'].get('predicted_price', listings[0]['reference'].get('base_price')) / 100
+        time.sleep(sleep_time)
+        buy_orders = get_buy_orders(listings[0])
+        filtered_buy_orders = [b for b in buy_orders if b['qty'] >= min_buy_orders_qty]
+        max_buy_order_price = filtered_buy_orders[0]['price'] / 100
+        time.sleep(sleep_time)
+        sales_history = get_sales_graph(skin)[:history_period]
+        avg_history_sold_count = int(sum([i['count'] for i in sales_history]) / history_period)
+        avg_history_sold_price = round(sum([i['avg_price'] for i in sales_history]) / history_period / 100, 2)
+        time.sleep(sleep_time)
+        doc = {
+            'fetch_date': str(datetime.datetime.now().date()),
+            'name': skin,
+            'sell_price': lowest_sell_price,
+            'estimated_price': estimated_price,
+            'buy_price': max_buy_order_price,
+            'sell_history_avg_qty': avg_history_sold_count,
+            'sell_history_avg_price': avg_history_sold_price
+        }
+        current_prices_data.append(doc)
+    return current_prices_data
