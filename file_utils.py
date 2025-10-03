ddef29fe42689919e6d76679ea5f73797af4be32
Tsk <dtyszkajr@gmail.com>
2025-10-03 16:25:23 -0300

    feat: added fields to the numerical parse step

--- file_utils.py
+++ file_utils.py
@@ -1,6 +1,7 @@
 
 import csv
 
+NUMERICAL_FIELDS = {'paid_price', 'probability'}
 ORDERS_BOOK_FPATH = "data/orders_book.csv"
 
 
@@ -10,7 +11,8 @@ def load_csv_data(fpath: str) -> list[dict]:
     with open(fpath, mode='r', newline='', encoding='utf-8') as csvfile:
         csv_reader = csv.DictReader(csvfile, delimiter=';', quotechar='"')
         for row in csv_reader:
-            if row.get('paid_price'):
-                row['paid_price'] = float(row['paid_price'])
+            for num_col in NUMERICAL_FIELDS:
+                if row.get(num_col):
+                    row[num_col] = float(row[num_col])
             data.append(row)
     return data
